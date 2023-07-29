728313889886eef359ba7cddcf1377f29fa4b446
Teodor Angelovski <angelovski.teo@outlook.com>
2023-07-29 20:39:02 +0200

    Implemented controller and handling requests

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 require '../bootstrap.php';
 
-header('Access-Control-Allow-Methods: GET,POST,DELETE');
+use Src\Repository\ProductRepository;
+use Src\Repository\ProductRepository\Implementation\ProductRepositoryImplementation;
+use Src\Service\ProductService;
+use Src\Service\ProductService\Implementation;
+use Src\Controller\ProductController;
+use Src\Controller\ProductController\Implementation\ProductControllerImplementation;
+
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE');
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Max-Age: 600');
@@ -10,3 +17,13 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
+$product_repository = new ProductRepositoryImplementation($db_connection);
+$product_service = new ProductServiceImplementation($product_repository);
+$product_controller = new ProductControllerImplementation($product_service);
+
+$url = explode('/', $url);
+if (!isset($url[1]) || $url[1] !== 'api') {
+    return json_encode($product_controller->invalidRequest('Invalid URL path'))
+}
+
+return json_encode($product_controller->processRequest($method));
---
+++ src/Controller/Api/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Src\Controller\Api;
+
+interface ProductController {
+    public function processRequest($request_method);
+    public function getAllProducts();
+    public function getProduct($sku);
+    public function createProduct($product);
+    public function updateProduct($product);
+    public function deleteProduct($sku);
+    public function deleteProducts($sku_list);
+    public function invalidRequest();
+}
